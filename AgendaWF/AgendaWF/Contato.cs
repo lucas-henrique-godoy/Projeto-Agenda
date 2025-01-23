49da57ccc9fef80fdb58f2de5735006bb59428ff
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2025-01-23 15:44:26 -0300

    Criei a classe modelo Contato e a classe de manipulação dos dados DALAgenda

---
+++ AgendaWF/AgendaWF/Contato.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgendaWF
+{
+    public class Contato
+    {
+        public int Id { get; set; }
+        public String Nome { get; set; }
+        public String Email { get; set; }
+        public String Telefone { get; set; }
+    }
+}
